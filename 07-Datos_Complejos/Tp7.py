e18afdf174a472a168b3ca80a00ed273005cd144
aguscassano <90217332+aguscassano@users.noreply.github.com>
2025-10-09 13:53:06 -0300

    Tp7 datos complejos finalizado

--- 07-Datos_Complejos/Tp7.py
+++ 07-Datos_Complejos/Tp7.py
@@ -172,3 +172,43 @@ while True:
 
     else:
         print("Entrada inválida, vuelva a intntarlo!")
+
+
+#9) Creá una agenda donde las claves sean tuplas de (día, hora) y los valores sean eventos.
+#Permití consultar qué actividad hay en cierto día y hora.
+
+
+agenda = {
+    ("lunes", "16:00"): "Clase Matemática",
+    ("martes", "12:00"): "Gimnasio",
+    ("miercoles", "8:00"): "Clase inglés",
+    ("jueves", "10:00"): "Turno dentista",
+    ("viernes", "21:00"): "Cumpleaños Lautaro"
+}
+
+dia = input("Ingrese el dia: ").strip().lower()
+hora = input("Ingrese la hora (ej: 16:00): ").strip()
+evento = agenda.get((dia, hora))
+
+if evento:
+    print(f"Actividad el dia {dia} a las {hora}: {evento}")
+else:
+    print("No hay actividad en la agenda para ese dia y hora.")
+
+
+#10) Dado un diccionario que mapea nombres de países con sus capitales, construí un nuevo 
+#diccionario donde: 
+#• Las capitales sean las claves. 
+#• Los países sean los valores. 
+
+paises_original = {"Argentina": "Buenos Aires", "Chile": "Santiago", "Brasil": "Brasilia", "Peru":"Lima"}
+
+print("Paises original:")
+for pais, capital in paises_original.items():
+    print(f"{pais}, {capital}")
+
+paises_invertido = {capital: pais for pais, capital in paises_original.items()}
+
+print("Paises invertidos:")
+for capital, pais in paises_invertido.items():
+    print(f"{capital}, {pais}")
